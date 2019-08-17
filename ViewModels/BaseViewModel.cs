5e09c11a3389a003fbe5a651a59b4fe18c9c3831
HideLord <polimonom@gmail.com>
2019-08-17 17:13:48 +0300

    Installed fody and added a base viewmodel which will be inherited by each and every newly generated viewmodel

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoVis.ViewModels
+{
+	[AddINotifyPropertyChangedInterface]
+	public abstract class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged = (sender,e) => { };
+	}
+}
