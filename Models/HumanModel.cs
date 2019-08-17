ec7caf0228382338b97dcaa334dbee9127e19185
HideLord <polimonom@gmail.com>
2019-08-17 19:58:02 +0300

    Created a basic human and a point class

---
+++ Models/HumanModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoVis.Models
+{
+	/// <summary>
+	/// A base class containing only mandatory info about the human object
+	/// </summary>
+    public abstract class HumanModel
+    {
+		public int Health { get; set; }
+		public Point Coords
+		{
+			get { return Coords; }
+			set{ Coords.copyFrom(value as Point);}
+		}
+    }
+}
---
+++ Models/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoVis.Models
+{
+	/// <summary>
+	/// Basic class containing information about a point.
+	/// What did you expect
+	/// </summary>
+    class Point
+    {
+		public int x { get; set; }
+		public int y { get; set; }
+		public Point(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+		public void copyTo(ref Point A)
+		{
+			A.x = x;
+			A.y = y;
+		}
+		public void copyFrom(ref Point A)
+		{
+			x = A.x;
+			y = A.y;
+		}
+	}
+}
