1ed78254b4ae666f2c480b55157b1035b98f45b4
HideLord <polimonom@gmail.com>
2019-08-21 19:56:51 +0300

    Commiting before I do something that breaks everything

--- ViewModels/MapViewModel.cs
+++ ViewModels/MapViewModel.cs
@@ -7,12 +7,20 @@ using System.Threading.Tasks;
 
 namespace AlgoVis.ViewModels
 {
+	public class CanvasNode
+	{
+		public int x { get; set; }
+		public int y { get; set; }
+		public string uri { get; set; }
+	}
+
 	public class MapViewModel : BaseViewModel
 	{
 		/// <summary>
 		/// Absolutely not sure if this will work or not! Needs testing...
 		/// </summary>
 		public Tile LevelMap { get; set; } // = Generator.GenerateMap(10, 1, 1);
+		public List<CanvasNode> CanvasNodes { get; set; }
 		public MapViewModel()
 		{
 			LevelMap = Generator.GenerateMapSpire(16, 5, 4, 5, 10);
@@ -24,8 +32,8 @@ namespace AlgoVis.ViewModels
 		/// </summary>
 		private void GenerateCanvasLayout()
 		{
-			Stack<Tile> TileStack = new Stack<Tile>();
-			TileStack.Push(LevelMap);
+			Stack<KeyValuePair<Tile, int>> TileStack = new Stack<KeyValuePair<Tile, int>>();
+			TileStack.Push(new KeyValuePair<Tile, int>(LevelMap,0));
 			while (TileStack.Count != 0)
 			{
 
